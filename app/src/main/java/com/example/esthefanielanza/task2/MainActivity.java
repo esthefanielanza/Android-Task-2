def762fd2ad95f9f4e0218a717071fecfa2eb0df
Esthafanie Lanza <esthefanielanza@gmail.com>
2017-06-22 01:24:05 -0300

    Cleaned the code and added an extra

--- app/src/main/java/com/example/esthefanielanza/task2/MainActivity.java
+++ app/src/main/java/com/example/esthefanielanza/task2/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.esthefanielanza.task2;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.woof);
+        ImageView dogeImage = (ImageView) findViewById(R.id.avatar_img);
+
+        dogeImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mp.start();
+            }
+        });
     }
 }
